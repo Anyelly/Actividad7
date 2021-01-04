372ab981bf8c03b44770886baf77c5f3daf7a7b7
Anyelly <aflorezr@ibero.edu.co>
2021-01-04 16:43:50 -0500

    Primer Commit

---
+++ Scripts/JQuery.js
@@ -0,0 +1,19 @@
+$('.h31').mousemove(function(){
+    $(this).css('background', 'pink')
+})
+
+$('.h31').click(function(){
+    $(this).css('color', 'grey')
+})
+
+$('.h31').mousemove(function(){
+    $(this).css('font-size', '20px')
+})
+
+$('.Primero').dblclick(function(){
+    $(this).css('background', 'cyan').css('width', '10%')
+})
+        
+$('.Primero').click(function(){
+    $(this).css('font-size', '10px')
+})
